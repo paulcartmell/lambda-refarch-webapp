e8f57de76338e1db0576a65d62ec306cd46ed0ae
Paul Cartmell <paul@starcode.co.uk>
2020-09-14 13:12:51 +0100

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "4vd1l9fudsnlferg3dgs7u9tia",     // CognitoClientID
-  "api_base_url": "https://ugl54sooxh.execute-api.eu-west-2.amazonaws.com/prod",                                     // TodoFunctionApi
+  "aws_user_pools_web_client_id": "6lcdu6cjn771afddnlgrodp665",     // CognitoClientID
+  "api_base_url": "https://ucerz6sg9i.execute-api.eu-west-2.amazonaws.com/prod",                                     // TodoFunctionApi
   "coginto_hosted_domain": "mytodoappdemo-starcode-todo.auth.eu-west-2.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://master.d16a26j8zppotx.amplifyapp.com"                                      // AmplifyURL
+  "redirect_url": "https://master.d11hkhxc75fylp.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
